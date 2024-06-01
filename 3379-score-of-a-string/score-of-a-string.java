b188002d5f28449c00873401f7bff9818bf22efb
casn99 <101577204+casn99@users.noreply.github.com>
2024-06-01 12:08:14 -0400

    Time: 1 ms (99.87%) | Memory: 42.2 MB (14.41%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.java
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -1,10 +1,10 @@
 class Solution {
     public int scoreOfString(String s) {
-        int t = 0;
+        int sum = 0;
         for(int i =0; i< s.length() -1; i++){
-            t += Math.abs(s.charAt(i) - s.charAt(i+1));
+            sum += Math.abs(s.charAt(i) - s.charAt(i+1));
         }
-        return t;
+        return sum;
 
     }
 }
