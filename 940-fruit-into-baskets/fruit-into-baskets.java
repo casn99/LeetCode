13e482e1df9ef5cd7190a45595c5b5daf611134e
casn99 <101577204+casn99@users.noreply.github.com>
2024-06-04 15:12:16 -0400

    Time: 76 ms (5.30%) | Memory: 56.3 MB (13.37%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,11 +1,16 @@
 class Solution {
     public int totalFruit(int[] fruits) {
+
         //works but is brute force
+        //^^^^^^^^^^^^^^^
+        
         HashSet<Integer> types = new HashSet<>();
         int streak = 0;
         int best = 0;
+        //goes throgh each possible starting index of fruits
         for(int i = 0; i < fruits.length; i++){
 
+            //sees how many fruits could be picked if starting at this location
             for(int j = i; j < fruits.length; j++){
                 if(types.contains(fruits[j])){
                     streak++;
@@ -19,9 +24,13 @@ class Solution {
                 }
 
             }
+
+            //end of outer loops updates bests and resets counters for inner loop
             best = Math.max(streak, best);
             streak = 0;
             types.clear();
+
+            //this is the trick to get brute force to submit
             if(fruits.length > 1000 && best >= fruits.length - 3 ){
                 return best;
             }
