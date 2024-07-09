e04e125ea5b89f62f7845a9c1a487d0c160f5683
casn99 <101577204+casn99@users.noreply.github.com>
2024-07-08 19:47:18 -0500

    Time: 4 ms (42.86%) | Memory: 72.9 MB (24.49%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.java
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -1,19 +1,21 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-        double total_waiting_time = 0;
-        int current_time = 0;
 
-        for (int[] customer : customers) {
-            int arrival = customer[0];
-            int service = customer[1];
-            if (current_time < arrival) {
-                current_time = arrival;
-            }
-            int waiting_time = current_time - arrival + service;
-            total_waiting_time += waiting_time;
-            current_time += service;
+        int movingtime = 0;
+        long timewaited = 0;
+
+        for(int i = 0; i < customers.length; i++){
+
+                if(movingtime < customers[i][0]){
+                    movingtime = customers[i][0];
+                }
+
+                timewaited += customers[i][1] + movingtime - customers[i][0];
+
+                movingtime += customers[i][1];
         }
 
-        return total_waiting_time / customers.length;
+        
+        return ((double)timewaited) / customers.length;
     }
 }
