fec41a5013138d375fdcf0045e01412420d1b4ed
casn99 <101577204+casn99@users.noreply.github.com>
2024-09-02 18:28:30 -0400

    Time: 1 ms (99.75%) | Memory: 55.1 MB (81.30%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,31 +1,14 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        
-        int[] result = new int[nums.length];
-        int total = 1;
-        int totalClean = 1;
-        int zeros = 0;
-
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] != 0){   
-                totalClean *= nums[i];
-            }else{
-                zeros++;
-            }
-                total *= nums[i];
-
-        }
-
-        for(int i = 0; i < nums.length; i++){
-            if(zeros > 1){
-                result[i] = 0;
-            }else if(nums[i] == 0){
-                result[i] = totalClean;
-            }else{
-                result[i] = total / nums[i];
-            }
-        }
-
-        return result;
+    int[] result = new int[nums.length];
+    for (int i = 0, tmp = 1; i < nums.length; i++) {
+        result[i] = tmp;
+        tmp *= nums[i];
     }
+    for (int i = nums.length - 1, tmp = 1; i >= 0; i--) {
+        result[i] *= tmp;
+        tmp *= nums[i];
+    }
+    return result;
+}
 }
