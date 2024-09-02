9b69993d1fb501160dfd03af8b6bd1dff0197a08
casn99 <101577204+casn99@users.noreply.github.com>
2024-09-02 12:40:27 -0400

    Time: 2 ms (72.59%) | Memory: 60.4 MB (13.01%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
@@ -3,11 +3,14 @@ class Solution {
         int j = 0;
         long totalRound = 0;
 
+        //figures out how many units used in one cycle
         for(int i = 0; i < chalk.length; i++){
             totalRound += chalk[i];
         }
 
-        k = (int)( k % totalRound );
+        //skip the max amounts of cycles
+        //will return an amount measurable in one cycle
+        k = (int)(k % totalRound);
 
         while(k >= chalk[j]){
             k -= chalk[j];
