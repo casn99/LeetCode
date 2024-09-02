85b7970b0719208bcac5abfd275f6e681684704b
casn99 <101577204+casn99@users.noreply.github.com>
2024-09-02 12:39:26 -0400

    Time: 2 ms (72.59%) | Memory: 60.1 MB (31.16%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
@@ -1,14 +1,20 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-        int i = 0;
-        while(k >= chalk[i]){
-            k -= chalk[i];
-            i++;
-            if(i == chalk.length){
-                i = 0;
-            }
+        int j = 0;
+        long totalRound = 0;
+
+        for(int i = 0; i < chalk.length; i++){
+            totalRound += chalk[i];
         }
-        return i;
+
+        k = (int)( k % totalRound );
+
+        while(k >= chalk[j]){
+            k -= chalk[j];
+            j++;
+        }
+
+        return j;
         
     }
 }
