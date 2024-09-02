7cca2aa9b00cafea102c2a5f22b4898bb45b5c12
casn99 <101577204+casn99@users.noreply.github.com>
2024-09-02 14:01:40 -0400

    Time: 26 ms (74.70%) | Memory: 68.1 MB (6.55%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i++){
+            set.add(nums[i]);
+        }
+
+        int j;
+        int temp = 0;
+        int best = 1;
+        for(int i : set){
+            if(!set.contains(i-1)){
+                temp = 1;
+                j = i+1;
+                while(set.contains(j)){
+                    temp++;
+                    j++;
+                }
+                best = Math.max(temp, best);
+            }
+           
+        }
+
+        return best;
+        
+    }
+}
