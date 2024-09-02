50e2c04948050b1a93ed8eafceff925b6a039dc5
casn99 <101577204+casn99@users.noreply.github.com>
2024-09-02 14:30:47 -0400

    Time: 5 ms (69.30%) | Memory: 57.8 MB (50.53%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -4,10 +4,11 @@ class Solution {
         int left = 0;
         int right = heights.length -1;
         int maxWater = -1;
+        int tempWater = 0;
 
 
         while(left < right){
-            int tempWater = Math.min(heights[left], heights[right]) * (right - left);
+            tempWater = Math.min(heights[left], heights[right]) * (right - left);
             maxWater = Math.max(maxWater, tempWater);
 
             if(heights[left] > heights[right]){
