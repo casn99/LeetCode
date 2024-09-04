b5c31ee0f1d442139741cb01cfa33efbeabc0ae0
casn99 <101577204+casn99@users.noreply.github.com>
2024-09-03 20:20:45 -0400

    Time: 8 ms (20.71%) | Memory: 43.7 MB (13.27%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int getLucky(String s, int k) {
+        String runner = "";
+        int temp = 0;
+        for(int j = 0; j < s.length(); j++){
+                runner = runner + Integer.toString( 1 + s.charAt(j) - 'a');
+            }
+        for(int i = 0; i < k; i++){         
+            for(int p =0; p < runner.length(); p++){
+                temp += runner.charAt(p) - '0';
+            }
+
+            s = Integer.toString(temp);
+            System.out.println(s);
+            runner = Integer.toString(temp);
+            temp =0;
+        }
+        return Integer.valueOf(s);
+    }
+}
