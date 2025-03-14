43ce92cda2234082079ffb83ad67d7d1f224b457
casn99 <101577204+casn99@users.noreply.github.com>
2025-03-14 11:13:58 -0400

    Time: 2 ms (84.81%) | Memory: 46.1 MB (46.86%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,13 +1,40 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        
         int left = 0;
-        for(int r = 0; r < nums.length; r++){
+        //int left = nums.length - 1;
+        
+
+        for(int r = 0; r < nums.length ; r++){
+        //for(int r = nums.length - 1; r >=0 ; r--){
+        
+            //this works because the left index saves the leftmost 0, that way we can swap it with any number to move it in line with the rest of the non zero numbers
+            // 0 1 0 3 12
+            // ^ saved
+            // 1 0 0 3 12
+            //   ^ saved
+            // 1 3 0 0 12
+            //     ^ saved
+            // 1 3 12 0 0
+            //        ^ saved
+
+    
             if(nums[r] != 0){
                 int temp = nums[r];
                 nums[r] = nums[left];
                 nums[left] = temp;
                 left++;
             }
+    /*
+            if(nums[r] != 0){
+                int temp = nums[r];
+                nums[r] = nums[left];
+                nums[left] = temp;
+                left--;
+            }
+    */
+            
+
         }
     }
 }
