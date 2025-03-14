e515248db7fda007511b7ad3137b69739b4e4117
casn99 <101577204+casn99@users.noreply.github.com>
2025-03-14 12:02:37 -0400

    Time: 13 ms (53.15%) | Memory: 42.8 MB (24.90%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         
+        /*
         int[] sM = new int[256];
         int[] tM = new int[256];
 
@@ -15,7 +16,21 @@ class Solution {
 
         }
         return true;
+        */
 
-        
+
+        HashMap<Character, Character> map = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+
+            if(map.containsKey(s.charAt(i)) || map.containsValue(t.charAt(i))){
+                if(map.getOrDefault(s.charAt(i), '0') != t.charAt(i)){
+                    return false;
+                }
+            }else{
+                map.put(s.charAt(i), t.charAt(i));
+            }
+        }
+        return true;
     }
 }
