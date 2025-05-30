eed3c5c51287c04aa3d50b0877d2df2e11404066
casn99 <101577204+casn99@users.noreply.github.com>
2025-05-30 19:54:32 -0400

    Time: 8 ms (86.80%) | Memory: 46.5 MB (83.94%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        if(intervals.length < 2){
+            return intervals;
+        }
+
+        ArrayList<int[]> answers = new ArrayList<>();
+
+
+
+        int[] temp = intervals[0];
+        for(int i = 1; i < intervals.length; i++){
+            if(temp[1] >= intervals[i][0]){
+                temp[1] = Math.max(temp[1],intervals[i][1]);
+            }else{
+                answers.add(temp);
+                temp = intervals[i];
+            }
+
+            if(i == intervals.length - 1){
+                answers.add(temp);
+                temp = intervals[i];
+            }
+        }
+
+        int[][] a = new int[answers.size()][2];
+        int i = 0;
+        for(int[] p : answers){
+            a[i]= answers.get(i);
+            i++;
+        }
+        return a;
+    }
+}
