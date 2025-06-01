352e2179ba7e3e6a5d6c166c91aab9628c1c1455
casn99 <101577204+casn99@users.noreply.github.com>
2025-06-01 11:41:02 -0400

    Time: 7 ms (65.29%) | Memory: 48.1 MB (45.26%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        //hashmap of alphabetized letters, if already exists gorup with
+        List<List<String>> groups = new LinkedList<>();
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
+            char[] word = s.toCharArray();
+            char[] sorted = s.toCharArray();
+            Arrays.sort(sorted);
+            String st = String.valueOf(sorted);
+            List<String> temp = map.getOrDefault(st, new LinkedList<String>());
+            temp.add(String.valueOf(word));
+            map.put(st, temp);
+        }
+        for(List<String> l : map.values()){
+            groups.add(l);
+        }
+
+        return groups;
+    }
+}
