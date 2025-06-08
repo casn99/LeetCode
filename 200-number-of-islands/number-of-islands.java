c541df26b34c99fd302fd6f388c4b0ca31e6bc23
casn99 <101577204+casn99@users.noreply.github.com>
2025-06-08 12:49:30 -0400

    Time: 4 ms (50.43%) | Memory: 50.2 MB (26.89%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -7,34 +7,30 @@ class Solution {
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[i].length; j++){
                 if(grid[i][j] == '1'){
-                    eliminateIsland(i, j, grid);
+                    eliminateIsland(grid, i, j);
                     numIslands++;
                 }
             }
         }
 
-
-
         return numIslands;
     }
 
-    private void eliminateIsland(int x, int y, char[][] g){
-
-        g[x][y] = '0';
-        
-        for(int[] d : directions){
-            int i = x + d[0];
-            int j = y + d[1];
-
-            if(i < 0 || j < 0 || i > g.length-1 ||j > g[i].length-1 || g[i][j] == '0'){
-                continue;
-            }else{
-                eliminateIsland(i, j, g);
-            }
-
+    private void eliminateIsland(char[][] g, int x, int y){
+        if(g[x][y] == '0'){
+            return;
         }
+        g[x][y] = '0';
 
-        return;
+        for(int[] d : directions){
+            if(x  + d[0] < 0 || y  + d[1] < 0 || x  + d[0] >= g.length || y  + d[1] >= g[x].length){
+
+            }else{
+                eliminateIsland(g, x + d[0], y + d[1]);
+            }
+        }
     }
 
+ 
+    
 }
