5554bae44e4afc22bfa21f54eb3c367453f73960
casn99 <101577204+casn99@users.noreply.github.com>
2025-06-09 10:27:10 -0400

    Time: 1 ms (94.75%) | Memory: 45.3 MB (82.52%) - LeetSync

---
+++ 419-battleships-in-a-board/battleships-in-a-board.java
@@ -0,0 +1,34 @@
+class Solution {
+    private int[][] directions = {{0,1},{1,0},{-1,0},{0,-1}};
+    public int countBattleships(char[][] grid) {
+        int numIslands = 0;
+
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                if(grid[i][j] == 'X'){
+                    eliminateIsland(grid, i, j);
+                    numIslands++;
+                }
+            }
+        }
+
+        return numIslands;
+    }
+
+    private void eliminateIsland(char[][] g, int x, int y){
+        if(g[x][y] == '.'){
+            return;
+        }
+        g[x][y] = '.';
+
+        for(int[] d : directions){
+            int c = x  + d[0];
+            int u = y + d[1];
+            if(c < 0 || u < 0 || c >= g.length || u >= g[x].length){
+                continue;
+            }
+            eliminateIsland(g, c, u);
+            
+        }
+    }
+}
